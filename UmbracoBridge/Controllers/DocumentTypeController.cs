18a40f57a078467ec84550c3cd29126093caec6b
Bernie-ramone <bernie_ramone@outlook.com>
2025-06-06 09:40:35 -0600

    add api controller

---
+++ UmbracoBridge/Controllers/DocumentTypeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UmbracoBridge.Models;
+using UmbracoBridge.Validators;
+
+namespace UmbracoBridge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DocumentTypeController : ControllerBase
+    {
+        [HttpGet("healthcheck")]
+        public async Task<IActionResult> Healthcheck()
+        {
+            
+            return Ok("Healthy");
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] DocumentTypeCreateRequest request)
+        {
+            var (isValid, errors) = DocumentTypeValidator.Validate(request);
+            if (!isValid)
+                return BadRequest(errors);
+
+            
+            return Ok("Document Type created (fake response)");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            
+            return Ok($"Deleted {id} (fake response)");
+        }
+    }
+}
